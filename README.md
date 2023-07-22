88d5396fadfc2324a4d89453a538c6aa91140e8e
kunal raj shah <rajkunal7279@gmail.com>
2023-07-22 23:40:28 +0530

    Make code with async and await

---
+++ README.md
@@ -0,0 +1,5 @@
+# Candy-Shop-Cart-Management
+Shopkeeper can add their Stock and also . when it click on button buy1,buy2,buy3 then according to clicked Button the stock is updated.
+Here i am save the data in CrudCrud and also get data after page loading.
+-- used HTML,CSS,JS --
+![candy](https://github.com/kunalrajshah/Candy-Shop-Cart-Management/assets/103275977/a1743415-aaf5-4400-92f8-907b32219ce6)
---
+++ script without await.js
@@ -0,0 +1,225 @@
+let candy = document.querySelector("#candy");
+let desc = document.querySelector("#desc");
+let price = document.querySelector("#price");
+let quantity = document.querySelector("#quant");
+let btn = document.querySelector("#button");
+let datatable = document.querySelector("#user-table");
+// CrudCrud url
+let url =
+  "https://crudcrud.com/api/985ef0c29f6540b1b39d225e69822fd4/candy_Shop";
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let row = document.createElement("tr");
+  let candycell = document.createElement("td");
+  let desccell = document.createElement("td");
+  let pricecell = document.createElement("td");
+  let quantitycell = document.createElement("td");
+  let optioncell = document.createElement("td");
+
+  // BUy button create
+  let buyone = document.createElement("button");
+  let buytwo = document.createElement("button");
+  let buythree = document.createElement("button");
+
+  buyone.textContent = "Buy One";
+  buytwo.textContent = "Buy Two";
+  buythree.textContent = "Buy Three";
+
+  buyone.classList.add("button1");
+  buytwo.classList.add("button1");
+  buythree.classList.add("button1");
+
+  // Value append in row
+  candycell.textContent = candy.value;
+  desccell.textContent = desc.value;
+  pricecell.textContent = price.value;
+  quantitycell.textContent = quantity.value;
+
+  optioncell.appendChild(buyone);
+  optioncell.appendChild(buytwo);
+  optioncell.appendChild(buythree);
+
+  row.appendChild(candycell);
+  row.appendChild(desccell);
+  row.appendChild(pricecell);
+  row.appendChild(quantitycell);
+  row.appendChild(optioncell);
+
+  datatable.appendChild(row);
+
+  // Send data to CrudCrud.
+  let obj = {
+    Candy: candy.value,
+    Description: desc.value,
+    Price: price.value,
+    Quantity: quantity.value,
+  };
+
+  axios
+    .post(url, obj)
+    .then((res) => {
+      console.log("Candy details is", res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // Updating quantity in cart according to buy candy.
+  buyone.addEventListener("click", () => {
+    if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+      quantitycell.textContent = quantitycell.textContent - 1;
+    else alert("No More Candy Availaible");
+  });
+  buytwo.addEventListener("click", () => {
+    if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+      quantitycell.textContent = quantitycell.textContent - 2;
+    else alert("No More Candy Availaible");
+  });
+  buythree.addEventListener("click", () => {
+    if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+      quantitycell.textContent = quantitycell.textContent - 3;
+    else alert("No More Candy Availaible");
+  });
+
+  // Update Data in crudcrud
+  buyone.addEventListener("click", getparentdata);
+  buytwo.addEventListener("click", getparentdata);
+  buythree.addEventListener("click", getparentdata);
+
+  function getparentdata(event) {
+    let data = event.target.parentElement.parentElement;
+    let name = data.children[0].textContent;
+    findDataId().then((res) => {
+      let filterdata = res.filter((eachrow) => eachrow.Candy === name);
+      let id = filterdata[0]._id;
+      update(id);
+    });
+  }
+
+  function update(id) {
+    axios.put(
+      `https://crudcrud.com/api/985ef0c29f6540b1b39d225e69822fd4/candy_Shop/${id}`,
+      {
+        Candy: candycell.textContent,
+        Description: desccell.textContent,
+        Price: pricecell.textContent,
+        Quantity: quantitycell.textContent,
+      }
+    );
+  }
+
+  // Function Get Id from CrudCrud
+  function findDataId() {
+    return axios
+      .get(url)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
+// On reload page we want data from crudcrud
+window.addEventListener("DOMContentLoaded", () => {
+  axios.get(url).then((res) => {
+    for (var i = 0; i < res.data.length; i++) {
+      let row = document.createElement("tr");
+      let candycell = document.createElement("td");
+      let desccell = document.createElement("td");
+      let pricecell = document.createElement("td");
+      let quantitycell = document.createElement("td");
+      let optioncell = document.createElement("td");
+
+      // BUy button create
+      let buyone = document.createElement("button");
+      let buytwo = document.createElement("button");
+      let buythree = document.createElement("button");
+
+      buyone.textContent = "Buy One";
+      buytwo.textContent = "Buy Two";
+      buythree.textContent = "Buy Three";
+
+      buyone.classList.add("button1");
+      buytwo.classList.add("button1");
+      buythree.classList.add("button1");
+
+      // Value append in row
+      candycell.textContent = res.data[i].Candy;
+      desccell.textContent = res.data[i].Description;
+      pricecell.textContent = res.data[i].Price;
+      quantitycell.textContent = res.data[i].Quantity;
+
+      optioncell.appendChild(buyone);
+      optioncell.appendChild(buytwo);
+      optioncell.appendChild(buythree);
+
+      row.appendChild(candycell);
+      row.appendChild(desccell);
+      row.appendChild(pricecell);
+      row.appendChild(quantitycell);
+      row.appendChild(optioncell);
+
+      datatable.appendChild(row);
+
+      // Updating quantity in cart according to buy candy.
+      buyone.addEventListener("click", () => {
+        if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+          quantitycell.textContent = quantitycell.textContent - 1;
+        else alert("No More Candy Availaible");
+      });
+      buytwo.addEventListener("click", () => {
+        if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+          quantitycell.textContent = quantitycell.textContent - 2;
+        else alert("No More Candy Availaible");
+      });
+      buythree.addEventListener("click", () => {
+        if (quantitycell.textContent !== 0 && quantitycell.textContent > 0)
+          quantitycell.textContent = quantitycell.textContent - 3;
+        else alert("No More Candy Availaible");
+      });
+
+      // Update Data in crudcrud
+      buyone.addEventListener("click", getparentdata);
+      buytwo.addEventListener("click", getparentdata);
+      buythree.addEventListener("click", getparentdata);
+
+      function getparentdata(event) {
+        let data = event.target.parentElement.parentElement;
+        let name = data.children[0].textContent;
+        findDataId().then((res) => {
+          let filterdata = res.filter((eachrow) => eachrow.Candy === name);
+          let id = filterdata[0]._id;
+          update(id);
+        });
+      }
+
+      function update(id) {
+        axios.put(
+          `https://crudcrud.com/api/985ef0c29f6540b1b39d225e69822fd4/candy_Shop/${id}`,
+          {
+            Candy: candycell.textContent,
+            Description: desccell.textContent,
+            Price: pricecell.textContent,
+            Quantity: quantitycell.textContent,
+          }
+        );
+      }
+
+      // Function Get Id from CrudCrud
+      function findDataId() {
+        return axios
+          .get(url)
+          .then((res) => {
+            return res.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+  });
+});
